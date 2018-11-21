f166962be19fc5bc70950967718473b7871f9f04
lyderic <lyderic@nuc.lyderic.net>
2018-11-21 17:22:26 +0000

    0.1.1: abstracted away sanitize to tools library

--- sanitize.go
+++ sanitize.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/lyderic/tools"
 )
@@ -18,53 +13,8 @@ func sanitizeAllFiles() {
 	var file string
 	for idx, file = range configuration.Files {
 		path := filepath.Join(basedir, file)
-		sanitizePath(idx, path)
+		tools.Sanitize(path, true)
 	}
 	n := idx + 1
 	fmt.Printf("%s %d file%s processed\n", bullet, n, tools.Ternary(n > 1, "s", ""))
-}
-
-func sanitizePath(idx int, path string) (err error) {
-	start := time.Now()
-	var nbytes int
-	if _, err = os.Stat(path); os.IsNotExist(err) {
-		return fmt.Errorf("cannot sanitize %s: file not found", path)
-	}
-	if nbytes, err = substitute(path,
-		"---", "***",
-		"--", "—",
-		"...", "…",
-		"« ", "« ",
-		" »", " »",
-		" ?", " ?",
-		" !", " !",
-		" ;", " ;",
-		" :", " :",
-		" \n", "\n",
-		"  ", " "); err != nil {
-		return err
-	}
-	if verbose {
-		fmt.Printf("[%03d] %s: processed %d bytes in %s\n", idx+1, filepath.Base(path), nbytes, time.Since(start))
-	}
-	return
-}
-
-func substitute(file string, replacements ...string) (n int, err error) {
-	r := strings.NewReplacer(replacements...)
-	var bb []byte
-	if bb, err = ioutil.ReadFile(file); err != nil {
-		return n, err
-	}
-	var f *os.File
-	if f, err = os.Create(file); err != nil {
-		return n, err
-	}
-	defer f.Close()
-	w := bufio.NewWriter(f)
-	if n, err = r.WriteString(w, string(bb)); err != nil {
-		return n, err
-	}
-	w.Flush()
-	return
 }
