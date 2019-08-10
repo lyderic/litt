fce1df40384f4edf058a3f5bb300d6bb76e22bd7
lydericlandry <lydericlandry@penguin>
2019-08-10 09:41:07 +0200

    0.5.9: improving 'init' command

--- cmd/initialize.go
+++ cmd/initialize.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var initializeCmd = &cobra.Command{
 	Use:   "init",
-	Short: fmt.Sprintf("Initialize a new project", PROGNAME),
+	Short: fmt.Sprintf("Initialize a %s new project", PROGNAME),
 	Run: func(cmd *cobra.Command, args []string) {
 		initialize()
 	},
@@ -18,10 +19,18 @@ var initializeCmd = &cobra.Command{
 }
 
 func initialize() {
-	configuration.Author = "Lydéric Landry"
-	configuration.Title = "Unset Title"
+	configuration.Author = viper.GetString("author")
+	configuration.Title = viper.GetString("title")
+	fmt.Printf("New project created with config file: %q\n", viper.GetString("config"))
+	fmt.Printf("Author: %s\n", viper.GetString("author"))
+	fmt.Printf("Title: %s\n", viper.GetString("title"))
 }
 
 func init() {
 	rootCmd.AddCommand(initializeCmd)
+	initializeCmd.Flags().StringP("author", "a", "Lydéric Landry", "set up author")
+	viper.BindPFlag("author", initializeCmd.Flags().Lookup("author"))
+	initializeCmd.Flags().StringP("title", "t", "", "(required) set up title")
+	initializeCmd.MarkFlagRequired("title")
+	viper.BindPFlag("title", initializeCmd.Flags().Lookup("title"))
 }
