6ca9bad7b580fa6aa83b55bb66aa3b82b43ff93d
lydericlandry <lydericlandry@penguin>
2019-08-10 10:08:14 +0200

    0.6.0: added option for using template in 'init' command

--- cmd/initialize.go
+++ cmd/initialize.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/lyderic/tools"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -24,6 +25,7 @@ func initialize() {
 	fmt.Printf("New project created with config file: %q\n", viper.GetString("config"))
 	fmt.Printf("Author: %s\n", viper.GetString("author"))
 	fmt.Printf("Title: %s\n", viper.GetString("title"))
+	fmt.Printf("%ssing template\n", tools.Ternary(viper.GetBool("template"), "U", "Not u"))
 }
 
 func init() {
@@ -33,4 +35,6 @@ func init() {
 	initializeCmd.Flags().StringP("title", "t", "", "(required) set up title")
 	initializeCmd.MarkFlagRequired("title")
 	viper.BindPFlag("title", initializeCmd.Flags().Lookup("title"))
+	initializeCmd.Flags().BoolP("template", "T", false, "use project template")
+	viper.BindPFlag("template", initializeCmd.Flags().Lookup("template"))
 }
