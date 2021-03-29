6684e2eeabfd948d4f7ed9585265e9ef2422b046
lyderic <lyderic@tintin.lyderic.net>
2021-03-29 16:34:21 +0100

    0.9.1 : improved init template

--- cmd/globals.go
+++ cmd/globals.go
@@ -1,7 +1,7 @@
 package cmd
 
 const (
-	VERSION                      = "0.9.0"
+	VERSION                      = "0.9.1"
 	PROGNAME                     = "litt"
 	EMPTY_FILE                   = 2
 	CONFIG_FILE_NOT_LOADABLE     = 4
